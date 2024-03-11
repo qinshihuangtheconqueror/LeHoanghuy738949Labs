00b2d2e3ef61aa56fcea40194714816e09f00192
Rody <huy.lh225976@sis.hust.edu.vn>
2024-03-11 08:58:16 +0700

    Complete assignment 6.2

---
+++ src/InputFromKeyboard.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class InputFromKeyboard {
+    public static void main(String args[]){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What's your name ?");
+        String strName = keyboard.nextLine();
+        int iAge = keyboard.nextInt();
+        System.out.println("How tall are you (m) ?");
+        double dHeight = keyboard.nextDouble();
+        System.out.println("Mrs/Ms. "+strName+ ", "+iAge+" years old."+ "Your Height is"+dHeight+".");
+
+    }
+}
