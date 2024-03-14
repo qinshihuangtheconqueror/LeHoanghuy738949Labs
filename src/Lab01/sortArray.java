dec16bd570bb03e6c9f1a5e268a5df08fe9105be
Rody <huy.lh225976@sis.hust.edu.vn>
2024-03-14 07:44:58 +0700

    Completed assignment 6.5

--- src/Lab01/sortArray.java
+++ src/Lab01/sortArray.java
@@ -2,7 +2,7 @@ package Lab01;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
+//zed
 public class sortArray {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
