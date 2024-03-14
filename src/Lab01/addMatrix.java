89b60e0dbbb5ba5de36fa1afe6a3e2ed263a5fac
Rody <huy.lh225976@sis.hust.edu.vn>
2024-03-14 07:45:40 +0700

    Completed assignment 6.6

--- src/Lab01/addMatrix.java
+++ src/Lab01/addMatrix.java
@@ -18,7 +18,7 @@ public class addMatrix {
             }
             System.out.println();
         }
-
+//matrix
         System.out.println("Enter the matrix b: ");
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
